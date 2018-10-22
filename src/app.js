5af87f77ac1ef541f5c763b02a7fe9e5a534ff77
Agnieszka Kochman <kochman.aga@gmail.com>
2018-10-22 22:40:10 +0200

    Step 3: New features (task 1)

--- src/app.js
+++ src/app.js
@@ -9,8 +9,11 @@ export class App {
 
     $('.load-username').on('click', function (e) {
       let userName = $('.username.input').val();
-     
-      fetchPolyfill('https://api.github.com/users/' + userName)
+
+      if (self.checkInput(userName)) {
+        self.markInputValid()
+
+        fetchPolyfill('https://api.github.com/users/' + userName)
         .then((response)=> {
           return response.json()
         })
@@ -18,7 +21,9 @@ export class App {
           self.profile = body;
           self.update_profile();
         })
-
+      } else {
+        self.markInputInvalid()
+      }
     })
 
   }
@@ -28,5 +33,31 @@ export class App {
     $('#profile-image').attr('src', this.profile.avatar_url)
     $('#profile-url').attr('href', this.profile.html_url).text(this.profile.login)
     $('#profile-bio').text(this.profile.bio || '(no information)')
+  }
+
+  checkInput(value) {
+    if (value.trim().length <= 0) {
+      return false
+    }
+
+    let pattern = /^[a-z0-9_-]+$/
+
+    if (!pattern.test(value)) {
+      return false
+    }
+
+    return true
+  }
+
+  markInputValid() {
+    if ($('.username.input').hasClass('invalid-input')) {
+      $('.username.input').removeClass('invalid-input')
+    }
+  }
+
+  markInputInvalid() {
+    if (!$('.username.input').hasClass('invalid-input')) {
+      $('.username.input').addClass('invalid-input')
+    }
   }
 }
